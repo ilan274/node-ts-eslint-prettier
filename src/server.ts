6c722ec870187394668ebd9de5799fb2a5de75fd
Ilan Herbach <ilan.herbach@gmail.com>
2021-09-22 07:08:47 -0300

    Initial template commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.get('/', (_request: Request, response: Response) => {
+  return response.status(200).json({
+    message: 'NodeJS template with TypeScript, ESLint and Prettier :)',
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
