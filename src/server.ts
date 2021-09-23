03616976b348308ccd0cb7a57b7fd1fcde277d9e
Ilan Herbach <ilan.herbach@gmail.com>
2021-09-22 22:38:11 -0300

    Set checkjs true

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
+
 app.get('/', (_request: Request, response: Response) => {
   return response.status(200).json({
     message: 'NodeJS template with TypeScript, ESLint and Prettier :)',
